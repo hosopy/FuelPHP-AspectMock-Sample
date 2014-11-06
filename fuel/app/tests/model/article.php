79e158d591ea93eda8f2451ec98df2bf5d11c2d4
hosopy <dev.hosopy@gmail.com>
2014-11-06 15:43:28 +0900

    Model_Articleのテスト

---
+++ fuel/app/tests/model/article.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Model_Articleのテスト
+ *
+ * @group Api
+ */
+
+class Test_Model_Article extends \Fuel\Core\TestCase
+{
+	/**
+	 * Model_Article::get_recommended() のテスト
+	 */
+	public function test_get_recommended()
+	{
+		// Articleを用意
+		for ($i = 0; $i < 5; $i++)
+		{
+			\Model_Article::forge(array(
+				'title' => "Title $i",
+				'body' => "Body $i",
+				'rank' => rand()
+			))->save();
+		}
+		
+		// rankの降順に並んでいることをテスト
+		$result = \Model_Article::get_recommended();
+		$this->assertCount(5, $result);
+		$response_ranks = \Arr::pluck($result, 'rank');
+		$prev_rank = PHP_INT_MAX;
+		foreach ($response_ranks as $rank)
+		{
+			$this->assertLessThanOrEqual($prev_rank, $rank);
+			$prev_rank = $rank;
+		}
+	}
+
+	protected function tearDown()
+	{
+		// DBをクリア
+		\DBUtil::truncate_table(Model_Article::get_table_name());
+	}
+}
